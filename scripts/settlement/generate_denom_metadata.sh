77ae185ddc6ad3842c8179709bb8d9573e5cec60
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-04-07 12:01:05 +0300

    chore: add scripts for advanced setup

---
+++ scripts/settlement/generate_denom_metadata.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ -z "$ROLLAPP_SETTLEMENT_INIT_DIR_PATH" ]; then
+    echo "ROLLAPP_SETTLEMENT_INIT_DIR_PATH is not set, using '$HOME/.rollapp_evm/init'"
+    ROLLAPP_SETTLEMENT_INIT_DIR_PATH="$HOME/.rollapp_evm/init"
+fi
+
+if [ ! -d "$ROLLAPP_SETTLEMENT_INIT_DIR_PATH" ]; then
+    mkdir -p "$ROLLAPP_SETTLEMENT_INIT_DIR_PATH"
+    echo "Creating the ROLLAPP_SETTLEMENT_INIT_DIR_PATH: $ROLLAPP_SETTLEMENT_INIT_DIR_PATH"
+else
+    echo "ROLLAPP_SETTLEMENT_INIT_DIR_PATH already exists: $ROLLAPP_SETTLEMENT_INIT_DIR_PATH"
+fi
+
+tee "$ROLLAPP_SETTLEMENT_INIT_DIR_PATH/denommetadata.json" >/dev/null <<EOF
+[
+  {
+    "description": "The native staking and governance token of the ${ROLLAPP_CHAIN_ID}",
+    "denom_units": [
+      {
+        "denom": "${BASE_DENOM}",
+        "exponent": 0
+      },
+      {
+        "denom": "${DENOM}",
+        "exponent": 18
+      }
+    ],
+    "base": "${BASE_DENOM}",
+    "display": "${DENOM}",
+    "name": "${DENOM}",
+    "symbol": "${DENOM}"
+  }
+]
+EOF
+set +x
