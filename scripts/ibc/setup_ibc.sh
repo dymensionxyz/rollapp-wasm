aea9aa9c2f012e8bde84e53392bbda805aa06287
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-07-23 19:23:27 +0300

    fix: ibc path should be initialized from the hub side

--- scripts/ibc/setup_ibc.sh
+++ scripts/ibc/setup_ibc.sh
@@ -128,7 +128,7 @@ fi
 
 echo '# -------------------------------- creating IBC link ------------------------------- #'
 
-rly paths new "$ROLLAPP_CHAIN_ID" "$SETTLEMENT_CHAIN_ID" "$RELAYER_PATH" --src-port "$IBC_PORT" --dst-port "$IBC_PORT" --version "$IBC_VERSION"
+rly paths new "$SETTLEMENT_CHAIN_ID" "$ROLLAPP_CHAIN_ID" "$RELAYER_PATH" --src-port "$IBC_PORT" --dst-port "$IBC_PORT" --version "$IBC_VERSION"
 
 rly tx link "$RELAYER_PATH" --src-port "$IBC_PORT" --dst-port "$IBC_PORT" --version "$IBC_VERSION" --max-clock-drift 70m
 # Channel is currently not created in the tx link since we changed the relayer to support on demand blocks
