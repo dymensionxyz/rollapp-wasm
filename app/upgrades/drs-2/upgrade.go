79443ff9df5d0133d257bc6833615bd498228dce
zale144 <aleksandar.sukovic@gmail.com>
2024-12-16 13:35:22 +0100

    Add DRS3 upgrade handler

--- app/upgrades/drs-2/upgrade.go
+++ app/upgrades/drs-2/upgrade.go
@@ -14,16 +14,19 @@ func CreateUpgradeHandler(
 	configurator module.Configurator,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		//migrate rollapp params with missing min-gas-prices and updating drs to 2
+		// migrate rollapp params with missing min-gas-prices and updating drs to 2
 		err := rpKeeper.SetVersion(ctx, uint32(2))
 		if err != nil {
 			return nil, err
 		}
-		err = rpKeeper.SetMinGasPrices(ctx, rollappparamstypes.DefaultParams().MinGasPrices)
-		if err != nil {
+		// first run drs-2 migration
+		if err := HandleUpgrade(ctx, rpKeeper); err != nil {
 			return nil, err
 		}
-
 		return mm.RunMigrations(ctx, configurator, fromVM)
 	}
+}
+
+func HandleUpgrade(ctx sdk.Context, rpKeeper rollappparamskeeper.Keeper) error {
+	return rpKeeper.SetMinGasPrices(ctx, rollappparamstypes.DefaultParams().MinGasPrices)
 }
