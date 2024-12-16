494ed575ebb2c7c490920075245fe967f1008dea
zale144 <aleksandar.sukovic@gmail.com>
2024-12-16 13:39:27 +0100

    small refactor

--- app/upgrades/drs-2/upgrade.go
+++ app/upgrades/drs-2/upgrade.go
@@ -14,12 +14,11 @@ func CreateUpgradeHandler(
 	configurator module.Configurator,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		// migrate rollapp params with missing min-gas-prices and updating drs to 2
-		err := rpKeeper.SetVersion(ctx, uint32(2))
-		if err != nil {
+		// upgrade drs to 2
+		if err := rpKeeper.SetVersion(ctx, uint32(2)); err != nil {
 			return nil, err
 		}
-		// first run drs-2 migration
+
 		if err := HandleUpgrade(ctx, rpKeeper); err != nil {
 			return nil, err
 		}
@@ -28,5 +27,6 @@ func CreateUpgradeHandler(
 }
 
 func HandleUpgrade(ctx sdk.Context, rpKeeper rollappparamskeeper.Keeper) error {
+	// migrate rollapp params with missing min-gas-prices
 	return rpKeeper.SetMinGasPrices(ctx, rollappparamstypes.DefaultParams().MinGasPrices)
 }
