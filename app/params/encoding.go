8cd66ca427a9428c78857378ab19e70617f31d73
Michael Tsitrin <114929630+mtsitrin@users.noreply.github.com>
2024-12-20 13:59:26 +0200

    fix(encoding): support eip712 encoding (#200)

--- app/params/encoding.go
+++ app/params/encoding.go
@@ -4,6 +4,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/codec/types"
+
+	simappparams "github.com/cosmos/cosmos-sdk/simapp/params"
 )
 
 // EncodingConfig specifies the concrete encoding types to use for a given app.
@@ -13,4 +15,16 @@ type EncodingConfig struct {
 	Codec             codec.Codec
 	TxConfig          client.TxConfig
 	Amino             *codec.LegacyAmino
+}
+
+// EncodingAsSimapp takes an EncodingConfig and returns a simappparams.EncodingConfig.
+// The only difference between the two is the type, which is useful for compatibility
+// between amino and protobuf implementations.
+func EncodingAsSimapp(encCfg EncodingConfig) simappparams.EncodingConfig {
+	return simappparams.EncodingConfig{
+		InterfaceRegistry: encCfg.InterfaceRegistry,
+		Codec:             encCfg.Codec,
+		TxConfig:          encCfg.TxConfig,
+		Amino:             encCfg.Amino,
+	}
 }
