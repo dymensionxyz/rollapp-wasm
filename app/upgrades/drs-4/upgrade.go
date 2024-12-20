2073e02a7a043da4740599851c64ea8a17f919a8
zale144 <aleksandar.sukovic@gmail.com>
2024-12-20 14:59:02 +0100

    feat(upgrade): add upgrade handler for DRS4 (#202)

--- app/upgrades/drs-4/upgrade.go
+++ app/upgrades/drs-4/upgrade.go
@@ -1,4 +1,4 @@
-package drs3
+package drs4
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -6,7 +6,7 @@ import (
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 	rollappparamskeeper "github.com/dymensionxyz/dymension-rdk/x/rollappparams/keeper"
 
-	drs2 "github.com/dymensionxyz/rollapp-wasm/app/upgrades/drs-2"
+	drs3 "github.com/dymensionxyz/rollapp-wasm/app/upgrades/drs-3"
 )
 
 func CreateUpgradeHandler(
@@ -15,23 +15,16 @@ func CreateUpgradeHandler(
 	configurator module.Configurator,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		if err := HandleUpgrade(ctx, rpKeeper); err != nil {
+		if rpKeeper.Version(ctx) < 3 {
+			// first run drs-3 migration
+			if err := drs3.HandleUpgrade(ctx, rpKeeper); err != nil {
+				return nil, err
+			}
+		}
+		// upgrade drs to 4
+		if err := rpKeeper.SetVersion(ctx, uint32(4)); err != nil {
 			return nil, err
 		}
 		return mm.RunMigrations(ctx, configurator, fromVM)
 	}
-}
-
-func HandleUpgrade(ctx sdk.Context, rpKeeper rollappparamskeeper.Keeper) error {
-	if rpKeeper.Version(ctx) < 2 {
-		// first run drs-2 migration
-		if err := drs2.HandleUpgrade(ctx, rpKeeper); err != nil {
-			return err
-		}
-	}
-	// upgrade drs to 3
-	if err := rpKeeper.SetVersion(ctx, uint32(3)); err != nil {
-		return err
-	}
-	return nil
 }
