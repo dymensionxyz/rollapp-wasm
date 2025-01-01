d8fbfaab71d4fa70f75ace8551d342e3e30c39a7
omritoptix <omritoptix@gmail.com>
2025-01-01 08:54:38 +0100

    Added DRS 6 upgrade handler.

--- app/app.go
+++ app/app.go
@@ -168,6 +168,7 @@ import (
 	drs3 "github.com/dymensionxyz/rollapp-wasm/app/upgrades/drs-3"
 	drs4 "github.com/dymensionxyz/rollapp-wasm/app/upgrades/drs-4"
 	drs5 "github.com/dymensionxyz/rollapp-wasm/app/upgrades/drs-5"
+	drs6 "github.com/dymensionxyz/rollapp-wasm/app/upgrades/drs-6"
 )
 
 const (
@@ -193,7 +194,7 @@ var (
 		rollappparamstypes.StoreKey,
 	}
 	// Upgrades contains the upgrade handlers for the application
-	Upgrades = []upgrades.Upgrade{drs2.Upgrade, drs3.Upgrade, drs4.Upgrade, drs5.Upgrade}
+	Upgrades = []upgrades.Upgrade{drs2.Upgrade, drs3.Upgrade, drs4.Upgrade, drs5.Upgrade, drs6.Upgrade}
 )
 
 func getGovProposalHandlers() []govclient.ProposalHandler {
