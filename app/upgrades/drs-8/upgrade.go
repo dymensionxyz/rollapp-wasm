fb4b99fc26ec29291beac6ef73e7c03a7905f6b8
Omri <omritoptix@gmail.com>
2025-01-19 17:01:35 +0200

    feat(drs): add support for DRS 8 (#230)
    Added support for DRS 8.
    
    (cherry picked from commit 1a5aa6aa919fed1f4119129f38f28e8cba28d067)

---
+++ app/upgrades/drs-8/upgrade.go
@@ -0,0 +1,37 @@
+package drs8
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	rollappparamskeeper "github.com/dymensionxyz/dymension-rdk/x/rollappparams/keeper"
+
+	drs7 "github.com/dymensionxyz/rollapp-wasm/app/upgrades/drs-7"
+)
+
+func CreateUpgradeHandler(
+	rpKeeper rollappparamskeeper.Keeper,
+	mm *module.Manager,
+	configurator module.Configurator,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		if err := HandleUpgrade(ctx, rpKeeper); err != nil {
+			return nil, err
+		}
+		return mm.RunMigrations(ctx, configurator, fromVM)
+	}
+}
+
+func HandleUpgrade(ctx sdk.Context, rpKeeper rollappparamskeeper.Keeper) error {
+	if rpKeeper.Version(ctx) < 7 {
+		// first run drs-7 migration
+		if err := drs7.HandleUpgrade(ctx, rpKeeper); err != nil {
+			return err
+		}
+	}
+	// upgrade drs to 8
+	if err := rpKeeper.SetVersion(ctx, uint32(8)); err != nil {
+		return err
+	}
+	return nil
+}
